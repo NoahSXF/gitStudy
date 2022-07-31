2ae9797ecf3205b43af3ad9277a55a3137afd126
NoahSXF <994518231@qq.com>
2022-07-31 13:56:05 +0800

    新建springboot项目

---
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
