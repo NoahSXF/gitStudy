c063835e80a3e47c04ae8a31c9cb720849ecfb49
NoahSXF <994518231@qq.com>
2022-11-29 20:26:42 +0800

    dhjsakhd

--- src/main/java/com/example/gitstudy/controller/TestController.java
+++ src/main/java/com/example/gitstudy/controller/TestController.java
@@ -17,4 +17,13 @@ public class TestController {
         map.put("王五", 21);
         return map;
     }
+
+    @RequestMapping("queryList")
+    public Map<String, Object> queryList2() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("张三", 18);
+        map.put("李四", 20);
+        map.put("王五", 21);
+        return map;
+    }
 }
